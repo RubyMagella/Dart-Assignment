9dadcc75afeea3b29713364db32ba7701ec0f8af
Ruby Itimu Magella <160042683+RubyMagella@users.noreply.github.com>
2024-03-10 16:08:23 +0300

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,25 @@
+void main(){
+  //Example of int data type
+  int age = 19;
+  print('Age:$age');
+
+  //Example of double data type
+  double height = 5.6;
+  print('Height:$height feet');
+
+  //Example of String data type
+  String name = 'John';
+  print('Name:$name');
+
+  //Example of List data type(List of intergers)
+  List<int>numbers = [4,5,6,7,8,9];
+  print('Numbers:$numbers');
+
+  //Example of Map data types(Mapping country to capital city)
+  Map<String,String> capitalcityMap = {
+  'Kenya': 'Nairobi',
+  'Uganda': 'Kampala',
+  'Egypt': 'Cairo',
+  };
+  print('Capital city Map: $capitalcityMap');
+}
